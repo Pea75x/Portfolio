1f02ca26bb61fddf88293c5bd3cd49cb90900418
Priya <priya_93_x@hotmail.co.uk>
2022-05-30 14:16:43 +0100

    first leaf is no longer covering projects

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.addEventListener('scroll', function () {
   const target = document.querySelectorAll('.scroll');
-  // const scrolled = window.pageYOffset;
-  // const rate = scrolled * 0.5;
-  // target.style.transform = `translate3d(0, ${rate}px, 0)`;
+  const leaf1 = document.querySelectorAll('.font-leaf');
+  const leaf2 = document.querySelectorAll('.leaf-2');
 
   let index = 0,
     length = target.length;
@@ -11,21 +10,25 @@ window.addEventListener('scroll', function () {
       let posX = window.pageYOffset * target[index].dataset.ratex;
       let posY = window.pageYOffset * target[index].dataset.ratey;
       let rotate = window.pageYOffset * target[index].dataset.rotate;
+      let position = target[index].dataset.position;
 
-      console.log(window.pageYOffset);
-      console.log(`x = ${posX}, y = ${posY}, rotate=${rotate}`);
       target[
         index
-      ].style.transform = `translate3d(${posX}px, ${posY}px, 0) rotate(${rotate}deg)`;
+      ].style.transform = `translate3d(${posX}px, ${posY}px, 0) rotate(${
+        rotate - position
+      }deg)`;
     } else {
       let posX = 481.5 * target[index].dataset.ratex;
       let posY =
         481.5 * target[index].dataset.ratey + window.pageYOffset - 481.5;
       let rotate = 481.5 * target[index].dataset.rotate;
+      let position = target[index].dataset.position;
 
       target[
         index
-      ].style.transform = `translate3d(${posX}px, ${posY}px, 0) rotate(${rotate}deg)`;
+      ].style.transform = `translate3d(${posX}px, ${posY}px, 0) rotate(${
+        rotate - position
+      }deg)`;
     }
   }
 });
